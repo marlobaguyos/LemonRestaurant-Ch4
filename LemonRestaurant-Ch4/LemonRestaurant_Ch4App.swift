55a01efd97df1d2803e9f02c494b0b39c23f06c6
marlo baguyos <marlo.baguyos@outlook.com>
2025-10-14 18:26:37 -0700

    Initial Commit

---
+++ LemonRestaurant-Ch4/LemonRestaurant_Ch4App.swift
@@ -0,0 +1,17 @@
+//
+//  LemonRestaurant_Ch4App.swift
+//  LemonRestaurant-Ch4
+//
+//  Created by Marlo baguyos on 10/14/25.
+//
+
+import SwiftUI
+
+@main
+struct LemonRestaurant_Ch4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LemonRestaurant-Ch4Tests/LemonRestaurant_Ch4Tests.swift
@@ -0,0 +1,17 @@
+//
+//  LemonRestaurant_Ch4Tests.swift
+//  LemonRestaurant-Ch4Tests
+//
+//  Created by Marlo baguyos on 10/14/25.
+//
+
+import Testing
+@testable import LemonRestaurant_Ch4
+
+struct LemonRestaurant_Ch4Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
